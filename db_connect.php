16f6d09d52ca550293edfebc5f604ae53c9b9202
Ekene Ikogbwe <broekeson@gmail.com>
2023-03-12 11:30:24 +0100

    update

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "user";
+$servername = "mysql";
+$username = "clouduser";
+$password = "cloud";
+$dbname = "altschool";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
